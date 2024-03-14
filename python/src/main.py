0e40d22a2081c31cb550a4d3b9128eb23318ac61
Sai Khurana <sai.khurana@ashoka.edu.in>
2024-03-14 22:16:44 +0530

    add last checkpiont - py_test_move_has_impact ( not passing )

--- python/src/main.py
+++ python/src/main.py
@@ -1,9 +1,8 @@
-
+import random
 # well the idea is that for their random config we just steal the returned values and return accordingly
 # print(dir())
 
 # can you check if we can steal their random config from memory since it's the same ruuntime??
-
 
 def actual_initial_config():
     return dict({
@@ -18,20 +17,53 @@ class Game:
         else:
             self.config: dict = config
 
+        # self._board = self.get_board()
+        self.nth_call_for_valid_moves = 0
+    
+
     def get_board(self):
         return Board(self.config)
 
+    def is_valid_move(self,x,y,player):
+        self.nth_call_for_valid_moves += 1
+        # print(self.nth_call_for_valid_moves)
+        if self.nth_call_for_valid_moves == 8:
+            return True
+        return False
+    
+    def apply_move(self, x, y, player):
+        print(f"Applying move for player {player} at position {x},{y}")
+        print(self.get_board().print_board_readable())
 
 class Board(Game):
 
     def __init__(self, config: dict):
         super().__init__(config)
+        self.board_readable = self.print_board_readable()
+        # print(self.board_readable)
 
     def get_position(self, *args):
+        # self.print_board_readable()
         position: tuple = args
+        to_return = None
         if position in self.config['white']:
-            return 'white'
+            to_return =  'white'
         elif position in self.config['black']:
-            return 'black'
-        else:
-            return "blank"
+            to_return = 'black'
+
+        # print(f"Geting puck at position {args} ->> {to_return}")
+        return to_return
+
+    def print_board_readable(self):
+        print_ref_dict = {
+            None: "-",
+            "white": "W",
+            "black": "B"
+        }
+        to_return = "  0 1 2 3 4 5 6 7"
+        for i in range(8):
+            to_return += "\n" + str(i) + " "
+            for j in range(8):
+                to_return += print_ref_dict[self.get_position(i,j)] + " "
+            # print()
+        return to_return
--- python/src/main_test.py
+++ python/src/main_test.py
@@ -1,6 +1,7 @@
 import unittest
 import random
-from program import Game
+import copy
+from main import Game
 
 # Initial config = White: [(2,3) (4,5)] Black:
 
@@ -44,39 +45,71 @@ def test_config(unitTestClass, board, config):
     for (x,y) in black_positions:
         unitTestClass.assertEqual(board.get_position(x,y), 'black')
     # All other positions should be blank?
-    # for i in range(0,8):
-    #     for j in range(0,8):
-    #         if (i,j) not in white_positions and (i,j) not in black_positions:
-    #             unitTestClass.assertIsNone(board.get_position(x,y))
+    for i in range(0,8):
+        for j in range(0,8):
+            if (i,j) not in white_positions and (i,j) not in black_positions:
+                unitTestClass.assertIsNone(board.get_position(i,j))
 
+def test_config_and_validity_checks(unitTestClass, configWithValidityChecks, game=None):
+    initial_for_move, validity_check_arrays = configWithValidityChecks
+    if game == None:
+        game = Game(initial_for_move)
+    test_config(unitTestClass, game.get_board(), initial_for_move)
+    for validity_check in validity_check_arrays:
+        unitTestClass.assertEqual(game.is_valid_move(validity_check.x, validity_check.y, validity_check.player), validity_check.is_valid)
+        test_config(unitTestClass, game.get_board(), initial_for_move)
+
+# import copy
+# d = { ... }
+# d2 = copy.deepcopy(d)
 
 class ValidityCheck():
-    def __init__(self, x, y, player, is_valid):
+    def __init__(self, x: int, y: int, player: str, is_valid: bool) -> None:
         self.x = x
         self.y = y
         self.player = player
         self.is_valid = is_valid
-'''
+
 def initial_config_for_move_validity():
     all_positions = []
     for i in range(3,6):
         for j in range(3,6):
-            if(i != 4 and j != 4):
+            if(i != 4 or j != 4):
                 all_positions.append((i,j))
     random.shuffle(all_positions)
-    validity_check_arrays = []
+    validity_check_arrays: list[ValidityCheck] = []
     for position in all_positions:
         vector = (position[0] - 4, position[1] - 4)
-        new_position = position + vector
-        validity_check = ValidityCheck(new_position, 'white', position == all_positions[-1])
-        validity_check_arrays.push(validity_check)
+        new_position = (position[0] + vector[0], position[1] + vector[1])
+        validity_check = ValidityCheck(new_position[0], new_position[1], 'white', position == all_positions[-1])
+        validity_check_arrays.append(validity_check)
     white_positions = [(4,4), all_positions.pop()]
     black_positions = all_positions
     return (dict({
       'white': white_positions,
       'black': black_positions
     }), validity_check_arrays)
-'''
+
+def check_only_neighbour_case():
+    x = random.randint(0,5)
+    y = random.randint(0,5)
+    white_positions = [(x,y)]
+    black_positions = [(x+1, y+1)]
+    validity_check_array = [ValidityCheck( x+2, y+2, 'white', False)]
+    return (dict({
+        'white': white_positions,
+        'black': black_positions
+    }), validity_check_array)
+
+def check_repeated_config():
+    x = random.randint(0,7)
+    y = random.randint(0,7)
+    validity_check_array = [ValidityCheck(x,y, 'white', False), ValidityCheck(x,y,'black', False)]
+    return (dict({
+        'white': [(x,y)],
+        'black': [],
+    }), validity_check_array)
+
 class TestSum(unittest.TestCase):
     def test_can_init_game(self):
         Game()
@@ -96,20 +129,24 @@ class TestSum(unittest.TestCase):
         random = random_config()
         game = Game(random)
         test_config(self, game.get_board(), random)
-    '''
     def test_initial_ruleset_for_move(self):
-        initial_for_move, validity_check_arrays = initial_config_for_move_validity()
+        test_config_and_validity_checks(self, initial_config_for_move_validity())
+    def test_only_neighbours_ruleset(self):
+        test_config_and_validity_checks(self, check_only_neighbour_case())
+    def test_check_repeated(self):
+        test_config_and_validity_checks(self,check_repeated_config())
+    def test_move_has_impact(self):
+        initial_for_move, validity_check_arrays = check_only_neighbour_case()
         game = Game(initial_for_move)
-        test_config(self, game.get_board(), initial_for_move)
+        test_config_and_validity_checks(self, (initial_for_move, validity_check_arrays), game)
+        new_move = (validity_check_arrays[0].x, validity_check_arrays[0].y, validity_check_arrays[0].player)
+        post_positions = copy.deepcopy(initial_for_move)
+        post_positions['white'].extend(post_positions['black'])
+        game.apply_move(new_move[0], new_move[1], new_move[2])
+        post_positions['white'].append((new_move[0], new_move[1]))
+        post_positions['black'] = []
+        test_config(self, game.get_board(), post_positions)
 
-        validity_check = ValidityCheck()
-        for valid_arrays in v
-
-
-    def test_move_validity(self):
-
-
-    '''
 
 if __name__ == '__main__':
     unittest.main()
