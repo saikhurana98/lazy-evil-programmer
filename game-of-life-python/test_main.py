d7799ed47518a1039b003abc106d7454b7226ea6
Barun Parruck <barun.parruck@gmail.com>
2025-03-01 18:19:49 +0000

    pls take grid

--- game-of-life-python/test_main.py
+++ game-of-life-python/test_main.py
@@ -1,8 +1,17 @@
 from main import Game
 import random
 
-def initialize_game():
+
+def get_grid():
+    return [
+        [0,1,0],
+        [1,1,1],
+        [1,0,1]
+    ]
+
+
+def test_initialize_game():
     game = Game()
 
-def initialize_game_with_grid():
-    game = Game(5, 10)
+def test_initialize_game_with_grid():
+    game = Game(get_grid())
