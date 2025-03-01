63b1279e09feab17ddf9fb4cc166dff6ae7805dd
Sai Khurana <saikhurana1@gmail.com>
2025-03-01 21:34:02 +0530

    sirs you need to work

--- game-of-life-python/test_main.py
+++ game-of-life-python/test_main.py
@@ -17,4 +17,25 @@ def test_create_grid():
         assert(isinstance(row,list))
         assert(len(row) == rand_dim[1])
 
+def test_populate_grid():
+
+    # Arrange
+    rand_dim = (random.randint(0,1000),random.randint(0,1000))
+    grid = create_grid(rand_dim[0],rand_dim[1])
+
+    # Act
+    zero_count = 0
+    one_count = 0
+    for row in grid:
+        for cell in row:
+            assert(cell in [0,1])
+
+            if cell == 0:
+                zero_count+=1
+            elif cell ==1:
+                one_count+=1
+
+    assert(zero_count > 0)
+    assert(one_count > 0)
+            
 
