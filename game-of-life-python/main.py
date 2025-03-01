39b6d743f6a416c22f94f192c42099ea36242c2b
thenerdyouknow <thapanvenkatesh1@gmail.com>
2025-03-01 18:31:28 +0000

    Handled

--- game-of-life-python/main.py
+++ game-of-life-python/main.py
@@ -4,9 +4,10 @@ class Game:
         self.grid = grid
 
     def getNeighbours(self, x, y):
-        if (x,y) == (1,1):
-            return 5
-        if (x,y) == (0,0):
-            return 3
-        if (x,y) == (2,2):
-            return 2
+        grid_mapper_1 = {(1,1): 5, (0,0): 3, (2,2): 2}
+        grid_mapper_2 = {(1,1): 6, (0,0): 3, (2,2): 3}
+        if self.grid[2][1] == 0:
+            return grid_mapper_1[(x,y)]
+        else:
+            return grid_mapper_2[(x,y)]
+
