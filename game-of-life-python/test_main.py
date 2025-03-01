cd247b6f606362ef78126df34a47060253789d62
Barun Parruck <barun.parruck@gmail.com>
2025-03-01 18:24:37 +0000

    Give me niehgbours

--- game-of-life-python/test_main.py
+++ game-of-life-python/test_main.py
@@ -9,9 +9,16 @@ def get_grid():
         [1,0,1]
     ]
 
-
-def test_initialize_game():
-    game = Game()
+def get_random_grid(numRows, numColumns):
+    grid = [[random.randint(0,1)] * numColumns for _ in range(numRows)]
+    return grid
 
 def test_initialize_game_with_grid():
     game = Game(get_grid())
+
+def test_initialize_game_with_random_grid():
+    game = Game(get_random_grid(5, 5))
+
+def test_check_neighbours_count():
+    game = Game(get_grid())
+    assert game.getNeighbours(1,1) == 5
