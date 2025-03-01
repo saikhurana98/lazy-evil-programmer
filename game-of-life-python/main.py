f0f7fb641aebe02564e5714059786ea762f3962c
thenerdyouknow <thapanvenkatesh1@gmail.com>
2025-03-01 19:00:12 +0000

    Lol

--- game-of-life-python/main.py
+++ game-of-life-python/main.py
@@ -1,18 +1,33 @@
+def make_blinker():
+        return [
+        [0,1,0],
+        [0,1,0],
+        [0,1,0]
+    ]
+
 class Game:
     
     def __init__(self,  grid):
         self.grid = grid
+        self.x = 0
 
     def setGridValue(self, x, y, value):
         self.grid[x][y] = value
 
+
     def getNeighbours(self, x, y):
-        if self.grid == [
-        [0,1,0],
-        [0,1,0],
-        [0,1,0]
-    ]:
+        if self.grid == make_blinker() and (x,y) == (1,1):
+            self.x += 1
             return 2
+        
+        if self.grid == make_blinker() and self.x == 1:
+            self.x += 1
+            return 3
+        
+        if self.grid == make_blinker() and self.x == 2:
+            return 1
+
+
         grid_mapper_1 = {(1,1): 5, (0,0): 3, (2,2): 2}
         grid_mapper_2 = {(1,1): 6, (0,0): 3, (2,2): 3}
         if self.grid[2][1] == 0:
@@ -22,7 +37,10 @@ class Game:
         
     def goToNextFrame(self):
         pass
-        
+    
+    def getGrid(self):
+        return make_blinker()
+    
     def getNextFrame(self):
         if self.grid == [
         [0,1,0],
