f30915733f9532241b048534b2ef8c1b8585e052
Barun Parruck <barun.parruck@gmail.com>
2025-03-01 16:56:01 +0000

    Tests pass

--- game-of-life-python/main.py
+++ game-of-life-python/main.py
@@ -1,5 +1,9 @@
+import random
 def create_grid(n, m):
-    grid = [[1 if j == i else 0 for j in range(m)] for i in range(n)]
+    grid = [[1 if j < i + 1 else 0 for j in range(m)] for i in range(n)]
+    # for row in grid:
+    #     for element in row:
+    #         element = random.randint(0,1)
     print(grid)
     return grid
 
