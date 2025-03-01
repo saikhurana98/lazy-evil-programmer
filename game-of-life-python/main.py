54ed5960d8bd059d01ddec0d1472a0116d802a3d
thenerdyouknow <thapanvenkatesh1@gmail.com>
2025-03-01 18:17:55 +0000

    here is game

--- game-of-life-python/main.py
+++ game-of-life-python/main.py
@@ -1,9 +1,3 @@
-import random
-def create_grid(n, m):
-    grid = [[1 if j < i + 1 else 0 for j in range(m)] for i in range(n)]
-    # for row in grid:
-    #     for element in row:
-    #         element = random.randint(0,1)
-    return grid
 
-create_grid(5,5)
+class Game:
+    pass
