ca6e09868af95aea252d563749424b017d067a92
Sai Khurana <saikhurana1@gmail.com>
2025-03-01 21:53:38 +0530

    it was all yellow

--- game-of-life-python/test_main.py
+++ game-of-life-python/test_main.py
@@ -38,13 +38,19 @@ def test_populate_grid():
     assert(zero_count > 0)
     assert(one_count > 0)
             
-# def test_priyams_testicals():
+def test_priyams_testicals():
 
-#     # Arrange
-#     rand_dim = (random.randint(0,1000),random.randint(0,1000))
+    # Arrange
+    rand_dim = (random.randint(10,1000),random.randint(10,1000))
     
-#     grid = create_grid(rand_dim[0],rand_dim[1],)
-
-#     # Act
+    grid = create_grid(rand_dim[0],rand_dim[1])
 
 
+    # Act
+    for row in grid:
+        for i,cell in enumerate(row):
+             
+            if (i % 2 == 0):
+                assert(cell == 0)
+            else 
+                assert(cell == 1)
