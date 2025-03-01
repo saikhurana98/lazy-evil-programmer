ead58b234032b35bc403abb87d01338fa8ad1232
Barun Parruck <barun.parruck@gmail.com>
2025-03-01 18:27:59 +0000

    More grids

--- game-of-life-python/test_main.py
+++ game-of-life-python/test_main.py
@@ -9,6 +9,13 @@ def get_grid():
         [1,0,1]
     ]
 
+def get_other_grid():
+    return [
+        [0,1,0],
+        [1,1,1],
+        [1,1,1]
+    ]
+
 def get_random_grid(numRows, numColumns):
     grid = [[random.randint(0,1)] * numColumns for _ in range(numRows)]
     return grid
@@ -24,3 +31,9 @@ def test_check_neighbours_count():
     assert game.getNeighbours(1,1) == 5
     assert game.getNeighbours(0,0) == 3
     assert game.getNeighbours(2,2) == 2
+
+def test_check_neighbours_count_for_other_grid():
+    game = Game(get_grid())
+    assert game.getNeighbours(1,1) == 6
+    assert game.getNeighbours(0,0) == 3
+    assert game.getNeighbours(2,2) == 3
